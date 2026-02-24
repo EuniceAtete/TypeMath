01cc8af94e2b19fa338300f6a2fc2b7f6fdd67b1
Eunice Atete <euniceatete0@gmail.com>
2026-02-24 23:47:25 +0200

    Use calc

--- source/index.js
+++ source/index.js
@@ -1,1 +1,7 @@
-console.log("Pebble 🪨");
+import { add, subtract, multiply, divide } from "./calculator";
+
+console.log("Math Toolkit 🧮");
+console.log("5 + 3 =", add(5, 3));
+console.log("10 - 4 =", subtract(10, 4));
+console.log("6 * 7 =", multiply(6, 7));
+console.log("20 / 4 =", divide(20, 4));
